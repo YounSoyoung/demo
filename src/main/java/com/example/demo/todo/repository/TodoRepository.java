804bd72f1a8a8d3a3be90eac90afeaa18191fa19
YounSoyoung <grace902853@gmail.com>
2022-11-19 17:14:36 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/todo/repository/TodoRepository.java
@@ -0,0 +1,26 @@
+package com.example.demo.todo.repository;
+
+import com.example.demo.todo.entity.ToDo;
+
+import java.util.List;
+
+//역할: 할 일 데이터를 CRUD한다.
+public interface TodoRepository {
+    //할 일 생성 기능
+
+    /**
+     * 할 일 데이터를 저장소에 저장하는 기능
+     * @param todo - 할 일 데이터의 집합
+     * @return - 저장 성공시 true, 실패 시 false 반환
+     */
+    boolean save(ToDo todo); //생성이 되고 나서는 성공했는지 못했는지 알려줘야함으로 boolean 타입
+
+    //할 일 목록 조회 기능
+    List<ToDo> findAll();
+
+    //할 일 개별조회 기능
+    ToDo findOne(long id);
+
+    //할일 삭제 기능
+    boolean remove(long id);
+}
